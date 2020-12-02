a334f6f06cdf51fe07f34579421e20b22b3ebd64
Esteban <esteban97srs@hotmail.com>
2020-12-01 21:18:13 -0300

    validations v3

--- Client/DivingClient/src/app/edit-diving-log/edit-diving-log.component.ts
+++ Client/DivingClient/src/app/edit-diving-log/edit-diving-log.component.ts
@@ -13,12 +13,12 @@ export class EditDivingLogComponent implements OnInit {
  
     EditForm = new FormGroup({
     DateLog: new FormControl('', [Validators.required]),
-    WindDirection: new FormControl('', [Validators.required]),
+    WindDirection: new FormControl('', [Validators.required,Validators.maxLength(15)]),
     WaveHeight: new FormControl('', [Validators.required]),
     SurfaceCurrent: new FormControl('', [Validators.required]),
     DiversNumber: new FormControl('', [Validators.required]),
     DivingPermit: new FormControl('', [Validators.required]),
-    Observations: new FormControl('', [Validators.required])
+    Observations: new FormControl('', [Validators.required,Validators.maxLength(35)])
   });
 
   constructor(private service: SharedService, private rutaActiva: ActivatedRoute) { }
